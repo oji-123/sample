3e355b55c49f0eb608c453283b8e6df3f5e532cd
basyobasyo <om1997518@gmail.com>
2022-05-03 10:59:27 +0900

    ビューファイルの作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,14 @@
+<link rel="stylesheet" href="{{ asset('css/posts.css') }}">
+
 <h1>トップページ</h1>
 
-{{ $posts }}
-
 @foreach ($posts as $post)
-    <p>これは {{ $post->id }} ユーザーです。</p>
+<div class="post">
+  <div class="post-date">
+    投稿日時：{{ $post->created_at }}
+  </div>
+  <div class="post-content">
+    {{ $post->content }}
+  </div>
+</div>
 @endforeach
